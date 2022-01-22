5306737462571d3ab78e074cde153e50f2c0819b
Shreejan Joshi <shreejanjoshi97@gmail.com>
2022-01-22 12:41:05 +0100

    made a connection with a database

---
+++ admin/includes/config.php
@@ -0,0 +1,15 @@
+<?php
+
+//database connection constants
+
+
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'gallery_db');
+
+$connection = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+
+if($connection){
+    echo "true";
+}
