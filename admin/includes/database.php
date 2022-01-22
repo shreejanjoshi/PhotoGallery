3f17e320e88f700248f5345f2a1b5199e893b79f
Shreejan Joshi <shreejanjoshi97@gmail.com>
2022-01-22 12:59:17 +0100

    made a database class

---
+++ admin/includes/database.php
@@ -0,0 +1,22 @@
+<?php
+
+require_once("new_config.php");
+
+class Database{
+
+    private $connection;
+
+    public function open_db_connection(){
+
+        $this->connection = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+
+        if(mysqli_connect_error()){
+            die("Database connection failed ". mysqli_error());
+        }
+    }
+
+}
+
+$database = new Database();
+$database->open_db_connection();
+
--- admin/includes/init.php
+++ admin/includes/init.php
@@ -1,3 +1,5 @@
 <?php
 
 include("new_config.php");
+
+include("database.php");
