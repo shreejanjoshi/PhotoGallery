a216688369de6b422b43fd7b45578ae48816ae10
Shreejan Joshi <shreejanjoshi97@gmail.com>
2022-01-26 10:58:23 +0100

    made a new class session to when app is on it can know user is logiin or not

--- admin/includes/user.php
+++ admin/includes/user.php
@@ -84,3 +84,42 @@ class User{
         return array_key_exists($the_attribute, $object_property);
     }
 }
+
+
+// User::find_all()  ... Here is the flow once is called
+
+// 1.   It goes to the find_all method
+
+// 2. The find_all() returns the find_by_query() results
+
+// 3. The find_by_query()  does a couple things
+
+//        1. it makes the query
+
+//         2. Fetches the the data from database table using  a while loop and it returns it in $row
+
+//         3. Passes the results ($row) to the Instantiation (instantantion - weird name I know) method
+
+//         4. Returns the object in the $the_object_array variable that it gets from the  instantantion method.
+
+// 5. And that will be the result that find_all() returns when we use User::find_all()
+
+
+
+//   What the Instantation method is doing
+
+//    1. Gets the calling class name.
+
+//    2. Creates an instance of the class
+
+//    3. It loops through the $the_record variable that has all the records
+
+//    4. It checks to see if the properties exist on that object with the other method has_the_property()
+
+//    5. If the keys from the record which basically are the columns from db table are found or equal the object properties then it assigns    the values to them.
+
+// 6. Finally it returns the object!
+
+
+
+// The purpose of this is to basically create our own API to deal with the database query so that in the future we can plug in other API's. Now there still a couple things I want to improve to make this way better, cleaner and more universal. 
