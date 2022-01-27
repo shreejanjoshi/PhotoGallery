f7f8582338a7b8055c438610bc51b10c3b71cdca
Shreejan Joshi <shreejanjoshi97@gmail.com>
2022-01-27 13:29:03 +0100

    getting vale from database but still have error in photoid

--- admin/photos.php
+++ admin/photos.php
@@ -1,4 +1,8 @@
 <?php include("includes/header.php"); ?>
+<?php if(!$session->is_signed_in()){ redirect("login.php");} ?>
+<?php
+    $photos = Photo::find_all();
+?>
 
     <!-- Navigation -->
     <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
@@ -41,13 +45,15 @@
                             </tr>
                             </thead>
                             <tbody>
+                            <?php foreach ($photos as $photo) : ?>
                             <tr>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
+                                <td><img src="http://placehold.it/62*62" alt=""></td>
+                                <td><?php echo $photo->id; ?></td>
+                                <td><?php echo $photo->filename; ?></td>
+                                <td><?php echo $photo->title; ?></td>
+                                <td><?php echo $photo->size; ?></td>
                             </tr>
+                            <?php endforeach; ?>
                             </tbody>
                         </table>
                     </div>
