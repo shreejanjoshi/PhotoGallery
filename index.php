40ef656cb5189ac0dc6c5729882d6ced23c075a3
Shreejan Joshi <shreejanjoshi97@gmail.com>
2022-02-01 09:06:03 +0100

    made paginate class trying to show page=1 with get methode in url

--- index.php
+++ index.php
@@ -2,6 +2,14 @@
 
 <?php
     $photos = Photo::find_all();
+
+    //page setting this by assigning get request
+    $page = !empty($_GET['page']) ? (int)$_GET['page'] : 1;
+
+    //4 item per and then newt page
+    $items_per_page = 4;
+
+    $items_total_count = Photo::count_all();
 ?>
 <div class="row">
 
@@ -12,10 +20,12 @@
 
             <?php  foreach ($photos as $photo):  ?>
                 <div class="col-xs-6 col-md-3">
-                    //link to go to that photo page
+                    <!-- link to go to that photo page -->
                     <a class="thumbnail" href="photo.php?id=<?php echo $photo->id; ?>">
                         <img class="home_page_photo img-responsive " src="admin/<?php echo $photo->picture_path(); ?>" alt="">
+                        <h4 class="text-center"><?php echo $photo->title; ?></h4>
                     </a>
+
                 </div>
             <?php endforeach; ?>
 
